084740d60df0e71012d6c1f8d8de2c006a1b2650
Anwyll Le <lelinhnguyen2109@gmail.com>
2022-12-07 22:54:18 -0500

    nested sum

--- test.js
+++ test.js
@@ -10,7 +10,27 @@
 // }
 // test(4);
 
+var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup",
+      },
+    },
+  };
+  
+  var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: "car" },
+  };
 
-let a = [];
-let b = [4];
-console.log(a.concat(b));
+
+  for(let char in obj1){
+    console.log(char);
+  }
